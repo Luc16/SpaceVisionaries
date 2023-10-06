176357d3b7b9b174bd40fe91fcf2a9e896ce70bf
ra247362 <r247362@dac.unicamp.br>
2023-10-06 20:41:01 -0300

    fix elliptical orbit

--- basic_orbit.js
+++ basic_orbit.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 export class BasicOrbit{
     constructor(scene, radiusA, radiusB){
         const path = new THREE.Path();
-        path.ellipse(0, 0, radiusB, radiusB, 0, Math.PI / 2, false);
+        path.ellipse(0, 0, radiusA, radiusB, 0, Math.PI / 2, false);
         path.ellipse(0, -radiusB, radiusA, radiusB, Math.PI / 2, Math.PI, false);
         path.ellipse(radiusA, 0, radiusA, radiusB, Math.PI, 3 * Math.PI / 2, false);
         path.ellipse(0, radiusB, radiusA, radiusB, 3 * Math.PI / 2, 2 * Math.PI, false);
