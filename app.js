97be5a3470f7965ea3df7ea20153d85fa465f215
pedroCarvalhoCintra <pedrocintra2@gmail.com>
2023-10-06 14:43:45 -0300

    add planets

--- app.js
+++ app.js
@@ -168,15 +168,60 @@ var InitDemo = function () {
 	//
 	// Create buffer
 	//
-	var sphere = createSphere(50);
-	var earth = new Planet(gl, program, sphere, 1, "http://localhost:8000/earth_texture.jpg");
 
-	var moon = new Planet(gl, program, sphere, 0.5, "http://localhost:8000/moon_texture.jpg");
-	moon.translate([3, 0, 0]);
+	const sunScale =  0.93/2;
 
-	var sun = new Planet(gl, program, sphere, 10, "http://localhost:8000/moon_texture.jpg");
-	sun.translate([0, 0,15]);
+	const mercuryScale = 0.0035*sunScale;
+	const venusScale = 0.0087*sunScale;
+	const earthScale = 0.0092*sunScale;
+	const marsScale = 0.0049*sunScale;
+	const jupiterScale = 0.1028*sunScale;
+	const saturnScale = 0.0866*sunScale;
+	const uranusScale = 0.0367*sunScale;
+	const neptuneScale = 0.0356*sunScale;
 
+	const sunMercuryDist = 0.39;
+	const sunVenusDist = 0.72;
+	const sunEarthDist = 1;
+	const sunMarsDist = 1.52;
+	const sunJupiterDist = 5.2;
+	const sunSaturnDist = 9.54;
+	const sunUranusDist = 19.2;
+	const sunNeptuneDist = 30.06;
+
+
+	const sphere = createSphere(50);
+	
+	var sun = new Planet(gl, program, sphere, sunScale, "http://localhost:8000/textures/sun_texture.jpg");
+	
+	var mercury = new Planet(gl, program, sphere, mercuryScale, "http://localhost:8000/textures/mercury_texture.jpg");
+	mercury.translate([sunMercuryDist, 0, 0])
+
+	var venus = new Planet(gl, program, sphere, venusScale, "http://localhost:8000/textures/venus_texture.jpg");
+	venus.translate([sunVenusDist, 0, 0])
+
+	var earth = new Planet(gl, program, sphere, earthScale, "http://localhost:8000/textures/earth_texture.jpg");
+	earth.translate([sunEarthDist, 0,0])
+
+	var mars = new Planet(gl, program, sphere, marsScale, "http://localhost:8000/textures/mars_texture.jpg");
+	mars.translate([sunMarsDist, 0, 0])
+
+	var jupiter = new Planet(gl, program, sphere, jupiterScale, "http://localhost:8000/textures/jupiter_texture.jpg");
+	jupiter.translate([sunJupiterDist, 0, 0])
+
+	var saturn = new Planet(gl, program, sphere, saturnScale, "http://localhost:8000/textures/saturn_texture.jpg");
+	saturn.translate([sunSaturnDist, 0, 0])
+
+	var uranus = new Planet(gl, program, sphere, uranusScale, "http://localhost:8000/textures/uranus_texture.jpg");
+	uranus.translate([sunUranusDist, 0, 0])
+
+	var neptune = new Planet(gl, program, sphere, neptuneScale, "http://localhost:8000/textures/neptune_texture.jpg");
+	neptune.translate([sunNeptuneDist, 0, 0])
+
+	gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.frontFace(gl.CCW);
+    gl.cullFace(gl.BACK);
 
 	// Tell OpenGL state machine which program should be active.
 	gl.useProgram(program);
@@ -186,7 +231,7 @@ var InitDemo = function () {
 	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
 
 	camera.setPerspective(glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
-	camera.lookAt([0, 0, -8], [0, 0, 0], [0, 1, 0]);
+	camera.lookAt([0, 0, -15], [0, 0, 0], [0, 1, 0]);
 
 	var worldMatrix = new Float32Array(16);
 	mat4.identity(worldMatrix);
@@ -201,8 +246,6 @@ var InitDemo = function () {
 	//
 	// Main render loop
 	//
-	var identityMatrix = new Float32Array(16);
-	mat4.identity(identityMatrix);
 	var angle = 0;
 
 	let then = 0;
@@ -218,18 +261,41 @@ var InitDemo = function () {
 		gl.clearColor(0.1, 0.1, 0.1, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-		angle = deltaTime / 6 * 2 * Math.PI;
-		earth.rotation[1] += angle;
-		earth.rotation[0] += angle/4;
-		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, earth.getModelMatrix());
-		earth.render(gl);
-
-		moon.rotation[2] += 3*angle;
-		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, moon.getModelMatrix());
-		moon.render(gl)
+		// angle = deltaTime / 6 * 2 * Math.PI;
+		// earth.rotation[1] += angle;
+		// earth.rotation[0] += angle/4;
 
 		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, sun.getModelMatrix());
 		sun.render(gl)
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, mercury.getModelMatrix());
+		mercury.render(gl);
+		
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, venus.getModelMatrix());
+		venus.render(gl);
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, earth.getModelMatrix());
+		earth.render(gl);
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, mars.getModelMatrix());
+		mars.render(gl);
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, jupiter.getModelMatrix());
+		jupiter.render(gl);
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, saturn.getModelMatrix());
+		saturn.render(gl);
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, uranus.getModelMatrix());
+		uranus.render(gl);
+
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, neptune.getModelMatrix());
+		neptune.render(gl);
+
+		// moon.rotation[2] += 3*angle;
+		// gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, moon.getModelMatrix());
+		// moon.render(gl)
+
 
 		requestAnimationFrame(loop);
 	};
