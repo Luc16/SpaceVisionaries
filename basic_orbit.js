b465a93a7b88e2e1d11890709d7fe80fff2803bc
ra247362 <r247362@dac.unicamp.br>
2023-10-08 16:54:16 -0300

    better orbits fix

--- basic_orbit.js
+++ basic_orbit.js
@@ -3,14 +3,6 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 
 export class BasicOrbit{
     constructor(scene, radiusA, radiusB, positionX, positionY, color, rotation=0){
-        // this.cur = new THREE.Path();
-        // this.cur.arcLengthDivisions = 2000;
-        // // this.path.ellipse(positionX, positionY, radiusA, radiusB, 0, Math.PI / 2, false);
-        // // this.path.ellipse(0, -radiusB, radiusA, radiusB, Math.PI / 2, Math.PI, false);
-        // // this.path.ellipse(radiusA, 0, radiusA, radiusB, Math.PI, 3 * Math.PI / 2, false);
-        // // this.path.ellipse(0, radiusB, radiusA, radiusB, 3 * Math.PI / 2, 2 * Math.PI, false);
-
-        // this.cur.ellipse(positionX, positionY, radiusA, radiusB, 0, 2*Math.PI, false, rotation);
 
         this.path = new THREE.EllipseCurve(positionX, positionY, radiusA, radiusB, 0, 2*Math.PI, false, rotation);
         this.path.arcLengthDivisions = 8000;
